28c1a21d665536b9f87fddb783a16737cdd06399
Andept@yandex.ru <Andept@yandex.ru>
2020-11-17 20:49:13 +0300

    Добавление управляемой камеры

---
+++ Scene1/Camera.h
@@ -0,0 +1,99 @@
+#define GLM_FORCE_RADIANS
+
+#include <iostream>
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+
+enum CameraDirection {
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+class Camera {
+public:
+	GLfloat cameraSpeed = 5.0f;
+	GLfloat sensitivity = 0.05f;
+
+	glm::vec3 position;
+	glm::vec3 front;
+	glm::vec3 right;
+	glm::vec3 up;
+	glm::vec3 worldFront;
+	glm::vec3 worldUp;
+
+	GLfloat yaw;
+	GLfloat pitch;
+	GLfloat fov;
+
+	Camera(glm::vec3 _position = glm::vec3(0.0f, 0.0f, 3.0f), 
+		   glm::vec3 _up = glm::vec3(0.0f, 1.0f, 0.0f), 
+		   glm::vec3 _front = glm::vec3(0.0f, 0.0f, -1.0f),
+		   GLfloat _yaw = -90.0f, GLfloat _pitch = 0.0f, GLfloat _fov = 45.0f) {
+
+		position = _position;
+		worldFront = _front;
+		worldUp = _up;
+
+		yaw = _yaw;
+		pitch = _pitch;
+		fov = _fov;
+
+		std::cout << "fov = " << fov;
+
+		RecalculateVectors();
+	}
+
+	void Move(CameraDirection dir, GLfloat deltaTime) {
+		GLfloat velocity = cameraSpeed * deltaTime;
+		if (dir == FORWARD)
+			position += front * velocity;
+		if (dir == BACKWARD)
+			position -= front * velocity;
+		if (dir == LEFT)
+			position -= right * velocity;
+		if (dir == RIGHT)
+			position += right * velocity;
+	}
+
+	void Rotate(GLfloat xoffset, GLfloat yoffset, GLboolean constrainPitch = true)
+	{
+		xoffset *= sensitivity;
+		yoffset *= sensitivity;
+
+		yaw += xoffset;
+		pitch -= yoffset;
+
+		if (constrainPitch)
+		{
+			if (pitch > 89.0f)
+				pitch = 89.0f;
+			if (pitch < -89.0f)
+				pitch = -89.0f;
+		}
+
+		RecalculateVectors();
+	}
+
+	void Zoom(bool state) {
+		if (state)
+			fov = 15.0f;
+		else
+			fov = 75.0f;
+	}
+
+private:
+	void RecalculateVectors() {
+		glm::vec3 _front;
+		_front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+		_front.y = sin(glm::radians(pitch));
+		_front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+
+		front = glm::normalize(_front);
+		right = glm::normalize(glm::cross(front, worldUp));
+		up = glm::normalize(glm::cross(right, front));
+	}
+};
--- Scene1/scene.cpp
+++ Scene1/scene.cpp
@@ -4,11 +4,13 @@
 #include <GL/glew.h>
 #include <glfw/glfw3.h>
 #include <iostream>
-#include <Shader/Shader.h>
 #include <SOIL/SOIL.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
+#include "Shader.h"
+#include "Camera.h"
 
 
 GLfloat triangle01[] = {
@@ -69,14 +71,47 @@ GLfloat cube01[] = {
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
+glm::vec3 cubePositions[] = {
+  glm::vec3(0.0f,  0.0f,  0.0f),
+  glm::vec3(2.0f,  5.0f, -45.0f),
+  glm::vec3(-1.5f, -2.2f, -2.5f),
+  glm::vec3(-3.8f, -2.0f, -12.3f),
+  glm::vec3(2.4f, -0.4f, -3.5f),
+  glm::vec3(-1.7f,  3.0f, -7.5f),
+  glm::vec3(1.3f, -2.0f, -2.5f),
+  glm::vec3(1.5f,  2.0f, -2.5f),
+  glm::vec3(1.5f,  0.2f, -1.5f),
+  glm::vec3(-1.3f,  1.0f, -1.5f)
+};
+
 GLuint indices[] = { 
 	0, 1, 3, 
 	1, 2, 3
 };
 
+bool firstMouseCallback = true;
+
+// Delta time
+GLfloat deltaTime = 0.0f;
+GLfloat lastFrame = 0.0f;
+
+// Mouse Positions
+GLfloat lastMouseX = 400, lastMouseY = 300;
+
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+// keys
+bool keys[1024];
+bool rightMouseFlag;
+
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void MouseCallback(GLFWwindow* window, double xpos, double ypos);
+void MouseButtonCallback(GLFWwindow* window, int key, int action, int mode);
+void Move();
+void Zoom();
+
 
 int main() {
-
 
 	int width, height;
 
@@ -96,13 +131,14 @@ int main() {
 
 
 	// Window
-	GLFWwindow* window = glfwCreateWindow(600, 600, "Beautiful scene", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(1200, 800, "Beautiful scene", nullptr, nullptr);
 	if (window == nullptr) {
 		std::cout << "Failed to create window." << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
 
@@ -183,25 +219,20 @@ int main() {
 	SOIL_free_image_data(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
-	/*glm::mat4 model;
-	model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));*/
-
 	glm::mat4 view;
-	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
 	glm::mat4 projection;
-	projection = glm::perspective<float>(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
-	
-
 
 	// Using key callbacks
-	void KeyCallback(GLFWwindow * window, int key, int scancode, int action, int mode);
 	glfwSetKeyCallback(window, KeyCallback);
+	glfwSetMouseButtonCallback(window, MouseButtonCallback);
+	glfwSetCursorPosCallback(window, MouseCallback);
 
 	// Game Loop
 	while (!glfwWindowShouldClose(window)) {
 
 		glfwPollEvents();
+		Move();
+		Zoom();
 
 		glClearColor(0.4f, 0.8f, 0.8f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -219,22 +250,26 @@ int main() {
 		//GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
 		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
-		// Transformation
-		/*glm::mat4 trans;
-		trans = glm::translate(trans, glm::vec3(sin((GLfloat)glfwGetTime() * 2.0f) / 2, 0.0f, 0.0f));
-		trans = glm::rotate(trans, (GLfloat)glfwGetTime() * 1.0f, glm::vec3(0.0f, 0.0f, 1.0f));*/
-		glm::mat4 model;
-		model = glm::rotate(model, (GLfloat)glfwGetTime() * 1.0f, glm::vec3(-0.5f, 0.2f, 1.0f));
+		// Transformations
+		//glm::mat4 view;
+		view = glm::lookAt(camera.position, camera.position + camera.front, camera.up);
+		//glm::mat4 projection;
+		projection = glm::perspective<float>(glm::radians(camera.fov), (float)width / height, 0.1f, 100.0f);
 
 		GLuint modelLoc = glGetUniformLocation(ourShader._program, "model");
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		GLuint viewLoc = glGetUniformLocation(ourShader._program, "view");
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 		GLuint projectionLoc = glGetUniformLocation(ourShader._program, "projection");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (int i = 0; i < 10; i++) {
+			glm::mat4 model;
+			model = glm::translate(model, cubePositions[i]);
+			model = glm::rotate(model, (GLfloat)glfwGetTime() * 1.0f, glm::vec3(-0.5f, 0.2f, 1.0f));
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
@@ -242,6 +277,11 @@ int main() {
 
 
 		glfwSwapBuffers(window);
+
+		// Calculation Delta Time
+		GLfloat currentFrame = (GLfloat)glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
 
 		// FPS Counter
 		currSecond = glfwGetTime();
@@ -262,11 +302,52 @@ int main() {
 }
 
 void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	if (action == GLFW_PRESS)
+		keys[key] = true;
+	else if (action == GLFW_RELEASE)
+		keys[key] = false;
+}
+
+void MouseButtonCallback(GLFWwindow* window, int key, int action, int mode) {
+	if (key == GLFW_MOUSE_BUTTON_RIGHT) {
+		if (action == GLFW_PRESS)
+			rightMouseFlag = true;
+		else
+			rightMouseFlag = false;
+	}
+}
+
+void MouseCallback(GLFWwindow* window, double xpos, double ypos) {
+
+	if (firstMouseCallback) {
+		lastMouseX = xpos;
+		lastMouseY = ypos;
+		firstMouseCallback = false;
 	}
 
+	GLfloat xoffset = xpos - lastMouseX;
+	GLfloat yoffset = ypos - lastMouseY;
+	lastMouseX = xpos;
+	lastMouseY = ypos;
 
+	camera.Rotate(xoffset, yoffset);
+}
 
+void Move() {
+	if (keys[GLFW_KEY_W])
+		camera.Move(FORWARD, deltaTime);
+	if (keys[GLFW_KEY_S])
+		camera.Move(BACKWARD, deltaTime);
+	if (keys[GLFW_KEY_A])
+		camera.Move(LEFT, deltaTime);
+	if (keys[GLFW_KEY_D])
+		camera.Move(RIGHT, deltaTime);
+}
 
+void Zoom() {
+	camera.Zoom(rightMouseFlag);
 }
